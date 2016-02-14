badd5c44d8188fc49421411b9004777bfc9878d0
LeafyDev <will09600@gmail.com>
2016-02-14 11:59:30 +0100

    Init

---
+++ Leafy-ImageUploader/Config.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 LeafyYT-Downloader INC. All rights reserved.
+// -----------------------------------------------------------
+
+#region
+
+using System;
+using System.IO;
+using Nini.Config;
+
+#endregion
+
+namespace Leafy_ImageUploader
+{
+    internal static class Config
+    {
+        private static IniConfigSource _configFile;
+
+        public static void Init()
+        {
+            if (!File.Exists("config.ini"))
+            {
+                CreateConfig();
+                Console.WriteLine("Config file not found, new config has been created, please fill in the info in config.ini.");
+                Console.WriteLine("Press any key to close.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+
+            ImgUploader.ftpAddr = ConfigSetting("[FTP]", "Address");
+            ImgUploader.ftpAcc = ConfigSetting("[FTP]", "Username");
+            ImgUploader.ftpPass = ConfigSetting("[FTP]", "Password");
+            ImgUploader.httpAddr = ConfigSetting("[HTTP]", "Address");
+        }
+
+        private static void CreateConfig()
+        {
+            try
+            {
+                var NewConfig = ";config.ini|[FTP]|Address = |Username = |Password = |[HTTP]|Address = ";
+                NewConfig = NewConfig.Replace("|", Environment.NewLine);
+                File.WriteAllText(@"config.ini", NewConfig);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+                Console.WriteLine("A config file could not be written, check current directory permissions.");
+                Console.ReadKey();
+                Environment.Exit(1);
+            }
+        }
+
+        private static string ConfigSetting(string key, string setting)
+        {
+            _configFile = new IniConfigSource("config.ini");
+
+            return _configFile.Configs[key].Get(setting);
+        }
+    }
+}
---
+++ Leafy-ImageUploader/ImgUploader.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 Leafy-ImageUploader INC. All rights reserved.
+// -----------------------------------------------------------
+
+#region
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+
+#endregion
+
+namespace Leafy_ImageUploader
+{
+    internal static class ImgUploader
+    {
+        internal static string ftpAcc = string.Empty;
+        internal static string ftpPass = string.Empty;
+        internal static string ftpAddr = string.Empty;
+        internal static string httpAddr = string.Empty;
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool OpenClipboard(IntPtr hWndNewOwner);
+
+        [DllImport("user32.dll")]
+        private static extern bool CloseClipboard();
+
+        [DllImport("user32.dll")]
+        private static extern bool SetClipboardData(uint uFormat, IntPtr data);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "Leafy Image Uploader 🍂";
+            Config.Init();
+
+            ShowWindow(GetConsoleWindow(), 0);
+
+            var FullName = args.FirstOrDefault();
+            var FileName = args.FirstOrDefault()?.Split('\\').LastOrDefault();
+            var FileExtension = FileName?.Split('.').LastOrDefault();
+            var NewFileName = DateTime.Now.ToString("dd-MMM-yy_HH:mm:ss") + "." + FileExtension;
+
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(ftpAcc, ftpPass);
+                if (FullName != null)
+                    client.UploadFile(ftpAddr + NewFileName, "STOR", FullName);
+            }
+
+            OpenClipboard(IntPtr.Zero);
+
+            if (!httpAddr.EndsWith("/"))
+            {
+                httpAddr = httpAddr + "/";
+            }
+            var URL = httpAddr + NewFileName;
+            Process.Start(URL);
+
+            var ptr = Marshal.StringToHGlobalUni(URL);
+            SetClipboardData(13, ptr);
+            CloseClipboard();
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
